b66726351159a738286842b96f4abcbf7640a130
Christoph Abert <christoph.abert@telekom.de>
2019-12-03 13:44:39 +0100

    Spring initializer

---
+++ m2Project/src/test/java/com/wildcodeschool/m2Project/M2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.m2Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
